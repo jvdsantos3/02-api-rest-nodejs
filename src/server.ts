fc37660fe43b30745628b529ba78ad2a87828f4e
João <jv.dossantos012@gmail.com>
2023-02-08 22:23:37 -0300

    Implementação de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
+import cookie from '@fastify/cookie'
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
